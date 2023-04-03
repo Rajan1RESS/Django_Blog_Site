092c1daad51ec844f55dff30e36ecccace755a35
Rajan Miletic <mynameisrajan21@gmail.com>
2023-04-03 23:20:30 +0200

    deleting section model

--- blog/articles/admin.py
+++ blog/articles/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from .models import Article,Section
+from .models import Article
 # Register your models here.
 
 
 # Register your models here.
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('name')
-
-admin.site.register(Article, ArticleAdmin)
-admin.site.register(Section)
+admin.site.register(Article)
+# admin.site.register(Section)
